cbe944bd6148f0e98306c2d66fd8e0c19253b49c
Egito <egito.salvador@gmail.com>
2022-06-21 22:35:34 -0300

    Finalizado cpt slider

--- html/wp-content/plugins/slider-salvador/cpt/class-slider-salvador-cpt.php
+++ html/wp-content/plugins/slider-salvador/cpt/class-slider-salvador-cpt.php
@@ -5,7 +5,8 @@ class Slider_Salvador_Post_Type
     function __construct()
     {
         add_action('init', array($this, 'slider_cpt'));
-        add_action('add_meta_boxes', array($this, 'add_meta_boxes'));
+        add_action('add_meta_boxes', array($this, 'slider_cpt_metabox'));
+        add_action('save_post', array($this, 'slider_cpt_save'), 10, 2);
     }
 
     public function slider_cpt()
@@ -36,7 +37,7 @@ class Slider_Salvador_Post_Type
             //            'register_meta_box_cb' => array($this, 'add_meta_boxes'), // registro de meta box -> campos complementares do post customizado
         ));
     }
-    public function add_meta_boxes()
+    public function slider_cpt_metabox()
     {
         add_meta_box(
             'slider-salvador-meta-box',
@@ -51,5 +52,39 @@ class Slider_Salvador_Post_Type
     public function add_inner_meta_boxes($post)
     {
         require_once(SS_PATH . 'views/slider-salvador-metabox.php');
+    }
+
+    public function slider_cpt_save($post_id)
+    {
+        // testar se o formulario é o mesmo que esta com nossos dados
+        if (isset($_POST['slider-salvador-nonce'])) {
+            if (!wp_verify_nonce($_POST['slider-salvador-nonce'], 'slider-salvador-nonce')) {
+                return;
+            }
+        }
+        // testar autosave do sistema
+        if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+            return;
+        }
+        // testar capacidades do usuario (estamos no cpt correto? e o usuario pode alterar?)
+        if (isset($_POST['post_type']) && $_POST['post_type'] === 'slider-salvador') {
+            if (!current_user_can('edit_page', $post_id)) {
+                return;
+            } elseif (!current_user_can('edit_post', $post_id)) {
+                return;
+            }
+        }
+        if (isset($_POST['action']) && $_POST['action'] == 'editpost') {
+            $old_text = get_metadata('post', $post_id, 'slider-salvador-link', true);
+            $new_text = sanitize_text_field($_POST['slider-salvador-link']);
+            $old_url = get_metadata('post', $post_id, 'slider-salvador-url', true);
+            $new_url = esc_url_raw($_POST['slider-salvador-url']);
+
+            $new_text = empty($new_text) ? 'Add some text' : $new_text;
+            $new_url = empty($new_url) ? 'Add some URL' : $new_url;
+
+            update_metadata('post', $post_id, 'slider-salvador-link', $new_text, $old_text);
+            update_metadata('post', $post_id, 'slider-salvador-url', $new_url, $old_url);
+        }
     }
 }
--- html/wp-content/plugins/slider-salvador/views/slider-salvador-metabox.php
+++ html/wp-content/plugins/slider-salvador/views/slider-salvador-metabox.php
@@ -1,14 +1,21 @@
+<?php
+$slider_link = get_post_meta($post->ID, 'slider-salvador-link', true);
+$slider_link = isset($slider_link) ? esc_html($slider_link) : '';
+$slider_url = get_post_meta($post->ID, 'slider-salvador-url', true);
+$slider_url = isset($slider_url) ? esc_url($slider_url) : '';
+?>
 <table class="form-table slider-salvador-metabox">
+    <input type="hidden" name="slider-salvador-nonce" value="<?php echo wp_create_nonce("slider-salvador-nonce"); ?>">
     <tr>
         <th><label for="slider-salvador-link">Link Text</label></th>
         <td>
-            <input type="text" class="regular-text link-text" name="slider-salvador-link" id="slider-salvador-link" value="" required>
+            <input type="text" class="regular-text link-text" name="slider-salvador-link" id="slider-salvador-link" value="<?php echo $slider_link; ?>" required>
         </td>
     </tr>
     <tr>
         <th><label for="slider-salvador-url">Link URL</label></th>
         <td>
-            <input type="url" class="regular-text link-url" name="slider-salvador-url" id="slider-salvador-url" value="" required>
+            <input type="url" class="regular-text link-url" name="slider-salvador-url" id="slider-salvador-url" value="<?php echo $slider_url; ?>" required>
         </td>
     </tr>
 </table>
